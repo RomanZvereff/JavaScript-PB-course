caf3b17ba6120a7b8414c86992f2712c7b69249d
RomanZvereff <romanzvereff@gmail.com>
2022-01-14 19:15:19 +0200

    task-10 done

--- Задача-10/task-10.js
+++ Задача-10/task-10.js
@@ -1,18 +1,18 @@
 /*
-#### Задача 9
-Отсортировать массив по убыванию.
+#### Задача 10
+Отсортировать массив по возрастанию.
 **Внимание**!
 - Не разрашается использовать специальные методы массивов.
 */
 
-var arr = [1,2,3,4,5,6];
+var arr = [6,5,4,3,2,1];
 var sorted = false;
 
 while(!sorted) {
     sorted = true;
     for(var i = 1; i <= arr.length; i++) {
         var temp;
-        if(arr[i - 1] < arr[i]) {
+        if(arr[i - 1] > arr[i]) {
             sorted = false;
             temp = arr[i];
             arr[i] = arr[i - 1];
