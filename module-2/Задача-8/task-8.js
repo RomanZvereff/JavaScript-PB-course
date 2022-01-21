cdd0a882ca07474c730c6a416687969a58d7bea0
RomanZvereff <romanzvereff@gmail.com>
2022-01-21 20:45:30 +0200

    module-2_task-8_done

---
+++ module-2/Задача-8/task-8.js
@@ -0,0 +1,37 @@
+/*
+####Задача 8
+Дан массив с числами `[1, 2, 3]`. Создайте функцию `f`, которая принимает данный массив в качестве параметров, 
+а затем последовательно выводит на экран его элементы. Обязательно нужно использовать рекурсию. 
+Использовать цикл запрещено. Данная функция должна обязательно содержать проверку входного параметра, 
+потому что принимать она может только не пустой массив.
+
+*Возможно вам понадобится метод [splice](https://developer.mozilla.org/uk/docs/Web/JavaScript/Reference/Global_Objects/Array/splice).*
+
+```js
+f([1,2,3]);
+// 1
+// 2
+// 3
+f(1,2,3) // Error: parameter type should be an array
+f('Content') // Error: parameter type should be an array
+f([]) // Error: parameter can't be an empty
+```
+*/
+
+var arr = [1, 2, 3, 7, 10];
+
+function f(arr) {
+    if(!Array.isArray(arr))
+        throw new Error("Error: parameter type should be an array");
+    if(arr.length == 0)
+        throw new Error("Error: parameter can't be an empty");
+
+    if(arr.length === 1) {
+        console.log(arr[0]);
+        return;
+    } else {
+        var element = arr.splice(0, 1)
+        console.log(element);
+        f(arr);
+    }
+}
