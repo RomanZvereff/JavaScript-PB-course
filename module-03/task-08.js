882dcd95c9e9de6e1c9f978cf8980745d2a3f190
RomanZvereff <romanzvereff@gmail.com>
2022-01-30 22:19:22 +0200

    module-03

--- module-03/task-08.js
+++ module-03/task-08.js
@@ -6,7 +6,7 @@
 Функция должна содержать проверки:
 - Первый параметр обязателен и может принимать только массив
 - Генерировать ошибку если на каком то уровне было найдено не число и не массив
-```javascript
+
 const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
 f(arr); // 12
 const arr2 = [[[[[1,2]]]]];
@@ -17,6 +17,27 @@ const arr4 = [[[[[]]]]];
 f(arr4); // 0
 const arr5 = [[[[[],3]]]];
 f(arr5); // 3
-```
 */
 
+function f(arr) {
+    if(!Array.isArray(arr)) {
+        throw new Error("Parameter is not an array");
+    }
+
+    let result = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        if(typeof arr[i] === 'number') {
+            result += arr[i];
+        }else if(Array.isArray(arr[i])) {
+            result += f(arr[i]);
+        } else {
+            throw new Error("Element is not a number or an array");
+        }
+    }
+
+    return result;
+}
+
+const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
+f(arr);
